268e400cda5a1977ded7869304032496bba578f3
Alex Zvorygin <grafetu@gmail.com>
2026-06-02 07:51:51 -0400

    Increase real_ai_vs_real_ai self-play to 13824 ticks

--- server/src/game/selfplay.rs
+++ server/src/game/selfplay.rs
@@ -1950,7 +1950,7 @@ fn real_ai_vs_real_ai() {
     const MIN_RIFLEMAN_TRAIN_COMMANDS: usize = 25;
     const MIN_ATTACK_MOVE_COMMANDS: usize = 13;
     const MIN_ATTACK_EVENTS: usize = 200;
-    const TICKS: u32 = 6_600;
+    const TICKS: u32 = 13_824;
 
     let players = vec![
         PlayerInit {
